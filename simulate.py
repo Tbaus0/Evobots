bf851db3fbfc992dee5b278946a7226b5118c60d
Tbaus0 <tobaus79@gmail.com>
2025-01-28 02:46:03 -0500

    tracking data

--- simulate.py
+++ simulate.py
@@ -1,7 +1,10 @@
 #import libraries
 import pybullet as p
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 import time
+import numpy
+import os
 
 physicsClient = p.connect(p.GUI)    #Connecting to physics engine
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  #To load URDF files i think
@@ -11,10 +14,17 @@ robotId = p.loadURDF("body.urdf")
 p.setGravity(0,0,-9.8)  #set gravity to -9.8
 
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+
+backLegSensorValues = numpy.zeros(10000)    #Create vector for detection
 
 for i in range(0,1000):
   p.stepSimulation()
+  backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg") #Checks for touch on BackLeg
   time.sleep(1/60)
-  print(i)
+
+file_path = os.path.join("data", "backLegSensorValues.npy")
+numpy.save(file_path, backLegSensorValues)
+print(backLegSensorValues)
 
 p.disconnect()
