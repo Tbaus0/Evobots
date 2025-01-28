f0231046d957b029f6a6e67889576057baab864a
Tbaus0 <tobaus79@gmail.com>
2025-01-27 23:22:58 -0500

    makes two boxes next to eachother

--- generate.py
+++ generate.py
@@ -1,5 +1,20 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+
+pyrosim.Start_SDF("boxs.sdf")
+
+#Cube Position
+x = 0
+y = 0.5
+z = 0
+
+#Setting block size
+length = 1
+width = 1
+height = 1
+
+#Generating cube1
+pyrosim.Send_Cube(name="Box", pos=[x,z,y] , size=[length,width,height])
+#Generate cube 2
+pyrosim.Send_Cube(name="Box2", pos=[1,0,1.5] , size=[1,1,1])
 
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,9 +1,15 @@
+#import libraries
 import pybullet as p
+import pybullet_data
 import time
 
-physicsClient = p.connect(p.GUI)
+physicsClient = p.connect(p.GUI)    #Connecting to physics engine
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  #To load URDF files i think
 
-p.loadSDF("box.sdf")
+planeId = p.loadURDF("plane.urdf")  #create floor
+p.setGravity(0,0,-9.8)  #set gravity to -9.8
+
+p.loadSDF("boxs.sdf")
 
 for i in range(0,1000):
   p.stepSimulation()
